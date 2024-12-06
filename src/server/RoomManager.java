ba627b3b6880a4ffbaa9b0a8203fb39f971d285f
김수현 <suhyun_113@naver.com>
2024-12-07 00:37:56 +0900

    Feat: 전체 방과 플레이어 관리 클래스 닉네임 중복 확인 및 플레이어 추가

--- src/server/RoomManager.java
+++ src/server/RoomManager.java
@@ -1,11 +1,28 @@
 package server;
 
-// 방 관리 클래스
-// 추후 구현 예정
+// 전체 방과 플레이어 관리 클래스
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RoomManager {
+    private final Map<String, Player> players; // 닉네임으로 플레이어 관리
+    private final Map<Integer, Room> rooms; // 방 ID로 방 관리
+    private int roomIdCounter; // 방 ID 생성용 카운터
+
     public RoomManager() {
-        System.out.println("RoomManager 초기화 완료");
+        players = new HashMap<>();
+        rooms = new HashMap<>();
+        roomIdCounter = 1;
+    }
+
+    // 닉네임 중복 확인 플레이어 추가
+    public boolean addPlayer(String nickname, Player player) {
+        if (players.containsKey(nickname)){
+            return false; // 닉네임 중복
+        }
+        players.put(nickname, player); // 닉네임으로 플레이어 정보 저장
+        return true;
     }
 }
 
