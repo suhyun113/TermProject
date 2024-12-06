dc973eda8fb04919f462abbce8223b85dff4701c
김수현 <suhyun_113@naver.com>
2024-12-07 01:49:58 +0900

    Feat: 플레이어 변수 및 메서드 추가

--- src/server/Player.java
+++ src/server/Player.java
@@ -7,11 +7,13 @@ import java.net.Socket;
 public class Player {
     private final String nickname; // 플레이어 닉네임
     private final Socket socket; // 플레이어와 연결된 소켓
+    private boolean isReady; // 플레이어의 준비 상태
 
     // Player 클래스 생성자
     public Player(String nickname, Socket socket) {
         this.nickname = nickname;
         this.socket = socket;
+        this.isReady = false;
     }
 
     // 플레이어 닉네임 반환하는 메서드
@@ -22,5 +24,13 @@ public class Player {
     // 플레이어와 연결된 소켓 반환하는 메서드
     public Socket getSocket() {
         return socket;
+    }
+
+    public boolean isReady() {
+        return isReady;
+    }
+
+    public void setReady(boolean ready) {
+        this.isReady = ready;
     }
 }
